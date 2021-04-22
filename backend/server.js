ef6d9267ee8ac41c47159d8c6a1fafa6d887dc0b
sruthiragupathy <sruthiragupathy@gmail.com>
2021-04-22 21:22:42 +0530

    Update port

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const address = require("./Database/address");
 // const femaleProducts = require("./Database/database");
 // const Product = require("./Database/Product");
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const bool = true;
 
