9c92cfc19919fc8ba82fc68bfe36ce7387ab262c
sruthiragupathy <sruthiragupathy@gmail.com>
2021-06-18 16:11:48 +0530

    style update

--- frontend/src/Components/ProductListingPage/MobileSortAndFilter.jsx
+++ frontend/src/Components/ProductListingPage/MobileSortAndFilter.jsx
@@ -76,74 +76,73 @@ export const MobileSortAndFilter = () => {
 			)}
 			{state.openFilter && (
 				<div className='filter-wrapper filter-wrapper-height'>
-					<div className='filter-container'>
-						<div className='common-filters filter-border-bottom'>
-							<div className='flex padding'>
-								<h4 className='rm'>FILTERS</h4>
-								<button
-									className='clear-all'
-									onClick={() => dispatch({ type: 'CLEAR_ALL_FILTERS' })}>
-									CLEAR ALL
-								</button>
-							</div>
-							<div className='individual-filter'>
+					<div className='common-filters filter-border-bottom'>
+						<div className='flex padding'>
+							<h4 className='rm'>FILTERS</h4>
+							<button
+								className='clear-all'
+								onClick={() => dispatch({ type: 'CLEAR_ALL_FILTERS' })}>
+								CLEAR ALL
+							</button>
+						</div>
+						<div className='individual-filter'>
+							<label>
+								<input
+									type='checkbox'
+									name='in_stock_only'
+									className='filter-margin'
+									checked={state.otherFilter.in_stock}
+									onChange={() =>
+										dispatch({ type: 'OTHER_FILTER', payload: 'in_stock' })
+									}
+								/>
+								In Stock Only
+							</label>
+						</div>
+						<div className='individual-filter'>
+							<label htmlFor='price'>
+								Price Range : 0 to {state.otherFilter.ranger_value}
+							</label>
+							<br />
+							<input
+								type='range'
+								min='0'
+								max='1000'
+								step='100'
+								value={state.otherFilter.ranger_value}
+								className='filter-margin'
+								onChange={(e) =>
+									dispatch({
+										type: 'OTHER_FILTER',
+										payload: 'ranger_value',
+										value: e.target.value,
+									})
+								}
+							/>
+						</div>
+					</div>
+
+					<h4 className='rm padding'>FILTER BY BRANDS</h4>
+					{getBrands(state.products).map((item, index) => {
+						return (
+							<div key={index} className='individual-filter'>
 								<label>
 									<input
 										type='checkbox'
-										name='in_stock_only'
+										name={item}
 										className='filter-margin'
-										checked={state.otherFilter.in_stock}
-										onChange={() =>
-											dispatch({ type: 'OTHER_FILTER', payload: 'in_stock' })
-										}
+										checked={state.brandFilter[item]}
+										onChange={() => {
+											dispatch({ type: 'FILTER_BY_BRAND', payload: item });
+										}}
 									/>
-									In Stock Only
+									{item}
 								</label>
 							</div>
-							<div className='individual-filter'>
-								<label htmlFor='price'>
-									Price Range : 0 to {state.otherFilter.ranger_value}
-								</label>
-								<br />
-								<input
-									type='range'
-									min='0'
-									max='1000'
-									step='100'
-									value={state.otherFilter.ranger_value}
-									className='filter-margin'
-									onChange={(e) =>
-										dispatch({
-											type: 'OTHER_FILTER',
-											payload: 'ranger_value',
-											value: e.target.value,
-										})
-									}
-								/>
-							</div>
-						</div>
+						);
+					})}
 
-						<h4 className='rm padding'>FILTER BY BRANDS</h4>
-						{getBrands(state.products).map((item, index) => {
-							return (
-								<div key={index} className='individual-filter'>
-									<label>
-										<input
-											type='checkbox'
-											name={item}
-											className='filter-margin'
-											checked={state.brandFilter[item]}
-											onChange={() => {
-												dispatch({ type: 'FILTER_BY_BRAND', payload: item });
-											}}
-										/>
-										{item}
-									</label>
-								</div>
-							);
-						})}
-					</div>
-					<div className='mobile-sort-and-filter__buttons apply-close'>
+					<div className='mobile-sort-and-filter__buttons'>
 						<button className='sort-btn' onClick={closeFilterHandler}>
 							CLOSE
 						</button>
